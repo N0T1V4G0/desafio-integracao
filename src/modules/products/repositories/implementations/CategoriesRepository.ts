13be5395e90abe97578cf66de40f951b5c7f7561
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-07 04:55:15 -0300

    'feat: implementa repositório de categoria e cria método create'

---
+++ src/modules/products/repositories/implementations/CategoriesRepository.ts
@@ -0,0 +1,13 @@
+import { Category } from '../../../../database/models/category';
+import { CreateCategoryDTO } from '../../dtos/category/CreateCategoryDTO';
+import { ICategory } from '../../entities/ICategory';
+import { ICategoriesRepository } from '../ICategoriesRepository';
+
+class CategoriesRepository implements ICategoriesRepository {
+  async create(category: CreateCategoryDTO): Promise<ICategory> {
+    const { codigo, titulo, status } = category;
+    return Category.create({ codigo, titulo, status }) as unknown as ICategory;
+  }
+}
+
+export { CategoriesRepository };
