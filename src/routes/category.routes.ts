e7a8c0e46b317600ae3a3700d48096d0306fd5e9
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-07 06:01:59 -0300

    feat: adiciona rota GET /category/:id

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { createCategoryController } from '../modules/products/use-cases/create-category';
+import { findCategoryController } from '../modules/products/use-cases/find-category';
 import { listCategoriesController } from '../modules/products/use-cases/list-categories';
 
 const categoryRouter = Router();
@@ -10,6 +11,10 @@ categoryRouter.post('/', (req, res, next) => {
 
 categoryRouter.get('/', (req, res, next) => {
   listCategoriesController.handle(req, res, next);
+});
+
+categoryRouter.get('/:id', (req, res, next) => {
+  findCategoryController.handle(req, res, next);
 });
 
 export { categoryRouter };
