b6de60ed7e05d19f0a4ce09791fccb5e480fe329
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-07 05:28:59 -0300

    feat: cria rota GET /category

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { createCategoryController } from '../modules/products/use-cases/create-category';
+import { listCategoriesController } from '../modules/products/use-cases/list-categories';
 
 const categoryRouter = Router();
 
 categoryRouter.post('/', (req, res, next) => {
   createCategoryController.handle(req, res, next);
+});
+
+categoryRouter.get('/', (req, res, next) => {
+  listCategoriesController.handle(req, res, next);
 });
 
 export { categoryRouter };
