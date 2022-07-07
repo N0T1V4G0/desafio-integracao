42d36385fdacfd75d1ff5418302bdab056f2b129
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-07 04:57:55 -0300

    feat: adiciona controller de criação de categoria e injeta dependências

---
+++ src/modules/products/use-cases/create-category/CreateCategoryController.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { CreateCategoryService } from './CreateCategoryService';
+
+class CreateCategoryController {
+  constructor(private createCategoryService: CreateCategoryService) {
+    this.createCategoryService = createCategoryService;
+  }
+
+  async handle(req: Request, res: Response, next: NextFunction) {
+    try {
+      const category = await this.createCategoryService.execute(req.body);
+      return res.status(201).json(category);
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  }
+}
+
+export { CreateCategoryController };
