afcfe3570a541264447245995b3b208f59be232a
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-08 07:42:13 -0300

    feat: cria rota GET /product

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { validateCreateProduct } from '../middlewares/validateCreateProduct';
 import { createProductController } from '../modules/products/use-cases/create-product';
+import { listProductsController } from '../modules/products/use-cases/list-products';
 
 const productRouter = Router();
 
 productRouter.post('/', validateCreateProduct, (req, res, next) => {
   createProductController.handle(req, res, next);
+});
+
+productRouter.get('/', (req, res, next) => {
+  listProductsController.handle(req, res, next);
 });
 
 export { productRouter };
