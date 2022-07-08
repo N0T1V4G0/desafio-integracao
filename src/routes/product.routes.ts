7f17281c8812ae1182d4c9b5d9e04aca1835a68b
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-08 07:29:32 -0300

    feat: add middleware de validação de criação de produto

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { validateCreateProduct } from '../middlewares/validateCreateProduct';
 import { createProductController } from '../modules/products/use-cases/create-product';
 
 const productRouter = Router();
 
-productRouter.post('/', (req, res, next) => {
+productRouter.post('/', validateCreateProduct, (req, res, next) => {
   createProductController.handle(req, res, next);
 });
 
