3125ba14fd882ed74730e80f5f379fcddf13c825
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-08 08:12:27 -0300

    feat: add rota GET product/:id

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { validateCreateProduct } from '../middlewares/validateCreateProduct';
 import { createProductController } from '../modules/products/use-cases/create-product';
+import { findProductController } from '../modules/products/use-cases/find-product';
 import { listProductsController } from '../modules/products/use-cases/list-products';
 
 const productRouter = Router();
@@ -11,6 +12,10 @@ productRouter.post('/', validateCreateProduct, (req, res, next) => {
 
 productRouter.get('/', (req, res, next) => {
   listProductsController.handle(req, res, next);
+});
+
+productRouter.get('/:id', (req, res, next) => {
+  findProductController.handle(req, res, next);
 });
 
 export { productRouter };
