e5b5475df67bcea1676c80c19da9c88e321cf3ed
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-11 23:42:45 -0300

    feat: add rota PATCH /product/:id/estoque/

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { validateCreateProduct } from '../middlewares/validateCreateProduct';
+import { validateUpdateInventory } from '../middlewares/validateUpdateInventory';
 import { validateUpdateProduct } from '../middlewares/validateUpdateProduct';
 import { createProductController } from '../modules/products/use-cases/create-product';
 import { deleteProductController } from '../modules/products/use-cases/delete-product';
 import { findInventoryController } from '../modules/products/use-cases/find-inventory';
 import { findProductController } from '../modules/products/use-cases/find-product';
 import { listProductsController } from '../modules/products/use-cases/list-products';
+import { updateInventoryController } from '../modules/products/use-cases/update-inventory';
 import { updateProductController } from '../modules/products/use-cases/update-products';
 
 const productRouter = Router();
@@ -33,5 +35,13 @@ productRouter.delete('/:id', (req, res, next) => {
 productRouter.get('/:id/estoque', (req, res, next) => {
   findInventoryController.handle(req, res, next);
 });
+
+productRouter.patch(
+  '/:id/estoque',
+  validateUpdateInventory,
+  (req, res, next) => {
+    updateInventoryController.handle(req, res, next);
+  },
+);
 
 export { productRouter };
