65a21b6fa7920c266641e97cbc893fbdb687c39f
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-12 01:45:57 -0300

    chore: add config postgres

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,23 +2,16 @@ const config = {
   development: {
     username: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || 'password',
-    database: 'desafio-db',
-    host: process.env.MYSQL_HOST || 'db',
+    database: process.env.MYSQL_DATABASE || 'mysql_db',
+    host: process.env.MYSQL_HOST || 'mysql_db',
     dialect: 'mysql',
   },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+  postgres: {
+    username: process.env.POSTGRES_USER || 'root',
+    password: process.env.POSTGRES_PASSWORD || 'password',
+    database: 'postgres_db',
+    host: process.env.POSTGRES_HOST || 'postgres_db',
+    dialect: 'postgres',
   },
 };
 
