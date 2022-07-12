320c96b7e33787dcb7155a69659fd2735e456734
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-12 02:32:50 -0300

    fix: corrige rota de produto e categoria

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use('/category', categoryRouter);
-app.use('/product', productRouter);
+app.use('/categorias', categoryRouter);
+app.use('/produtos', productRouter);
 app.use(errorMiddleware);
 
 export { app };
