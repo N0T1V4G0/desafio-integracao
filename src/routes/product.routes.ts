f724313526507002a343af2067279c7a370a2ddb
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-11 23:04:25 -0300

    feat: add rota GET /product/:id/estoque

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -3,6 +3,7 @@ import { validateCreateProduct } from '../middlewares/validateCreateProduct';
 import { validateUpdateProduct } from '../middlewares/validateUpdateProduct';
 import { createProductController } from '../modules/products/use-cases/create-product';
 import { deleteProductController } from '../modules/products/use-cases/delete-product';
+import { findInventoryController } from '../modules/products/use-cases/find-inventory';
 import { findProductController } from '../modules/products/use-cases/find-product';
 import { listProductsController } from '../modules/products/use-cases/list-products';
 import { updateProductController } from '../modules/products/use-cases/update-products';
@@ -27,6 +28,10 @@ productRouter.patch('/:id', validateUpdateProduct, (req, res, next) => {
 
 productRouter.delete('/:id', (req, res, next) => {
   deleteProductController.handle(req, res, next);
+});
+
+productRouter.get('/:id/estoque', (req, res, next) => {
+  findInventoryController.handle(req, res, next);
 });
 
 export { productRouter };
