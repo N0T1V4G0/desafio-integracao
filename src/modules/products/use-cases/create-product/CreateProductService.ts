58207cf817f558eb687de3928f96ef394735fa75
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-07-11 22:28:47 -0300

    feat: ao criar um produto, um inventário também é criado

--- src/modules/products/use-cases/create-product/CreateProductService.ts
+++ src/modules/products/use-cases/create-product/CreateProductService.ts
@@ -1,21 +1,25 @@
 import { AppError } from '../../../../errors/AppError';
 import { CreateProductDTO } from '../../dtos/product/CreateProductDTO';
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+import { IInventoryRepository } from '../../repositories/IInventoryRepository';
 import { IProductsRepository } from '../../repositories/IProductsRepository';
 
 class CreateProductService {
-  // private productsRepo: IProductsRepository;
-  // private categoriesRepo: ICategoriesRepository;
   constructor(
     private productsRepo: IProductsRepository,
     private categoriesRepo: ICategoriesRepository,
+    private inventoryRepo: IInventoryRepository,
   ) {}
 
   async execute(product: CreateProductDTO) {
     const { idCategoria } = product;
     const categoryExists = await this.categoriesRepo.find(idCategoria);
     if (!categoryExists) throw new AppError('Category not found', 404);
-    return this.productsRepo.create(product);
+    const newProduct = await this.productsRepo.create(product);
+    if (newProduct.id) {
+      await this.inventoryRepo.create(newProduct.id);
+    }
+    return newProduct;
   }
 }
 
--- src/modules/products/use-cases/create-product/index.ts
+++ src/modules/products/use-cases/create-product/index.ts
@@ -1,13 +1,16 @@
 import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+import { InventoryRepository } from '../../repositories/implementations/InventoryRepository';
 import { ProductsRepository } from '../../repositories/implementations/ProductsRepository';
 import { CreateProductController } from './CreateProductController';
 import { CreateProductService } from './CreateProductService';
 
 const productsRepository = new ProductsRepository();
 const categoriesRepository = new CategoriesRepository();
+const inventoryRepository = new InventoryRepository();
 const createProductService = new CreateProductService(
   productsRepository,
   categoriesRepository,
+  inventoryRepository,
 );
 const createProductController = new CreateProductController(
   createProductService,
